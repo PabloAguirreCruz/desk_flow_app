6d70b4d780b62b356ff81c31de452c855c44d16d
PabloAguirreCruz <p.cruz8@yahoo.com>
2026-05-10 11:32:40 -0400

    Add CRUD views, comment form, and tag association

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,19 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+
     path('tickets/', views.ticket_index, name='ticket-index'),
     path('tickets/<int:ticket_id>/', views.ticket_detail, name='ticket-detail'),
+    path('tickets/create/', views.TicketCreate.as_view(), name='ticket-create'),
+    path('tickets/<int:pk>/update/', views.TicketUpdate.as_view(), name='ticket-update'),
+    path('tickets/<int:pk>/delete/', views.TicketDelete.as_view(), name='ticket-delete'),
+    path('tickets/<int:ticket_id>/add-comment/', views.add_comment, name='add-comment'),
+    path('tickets/<int:ticket_id>/associate-tag/<int:tag_id>/', views.associate_tag, name='associate-tag'),
+    path('tickets/<int:ticket_id>/remove-tag/<int:tag_id>/', views.remove_tag, name='remove-tag'),
+
+    path('tags/', views.TagList.as_view(), name='tag-index'),
+    path('tags/<int:pk>/', views.TagDetail.as_view(), name='tag-detail'),
+    path('tags/create/', views.TagCreate.as_view(), name='tag-create'),
+    path('tags/<int:pk>/update/', views.TagUpdate.as_view(), name='tag-update'),
+    path('tags/<int:pk>/delete/', views.TagDelete.as_view(), name='tag-delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Ticket
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Ticket, Tag
+from .forms import CommentForm
 
 
 def home(request):
@@ -17,4 +20,67 @@ def ticket_index(request):
 
 def ticket_detail(request, ticket_id):
     ticket = Ticket.objects.get(id=ticket_id)
-    return render(request, 'tickets/detail.html', {'ticket': ticket})
+    comment_form = CommentForm()
+    tags_ticket_doesnt_have = Tag.objects.exclude(id__in=ticket.tags.all().values_list('id'))
+    return render(request, 'tickets/detail.html', {
+        'ticket': ticket,
+        'comment_form': comment_form,
+        'tags': tags_ticket_doesnt_have,
+    })
+
+
+class TicketCreate(CreateView):
+    model = Ticket
+    fields = ['title', 'description', 'priority', 'status']
+
+
+class TicketUpdate(UpdateView):
+    model = Ticket
+    fields = ['title', 'description', 'priority', 'status']
+
+
+class TicketDelete(DeleteView):
+    model = Ticket
+    success_url = '/tickets/'
+
+
+def add_comment(request, ticket_id):
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        new_comment = form.save(commit=False)
+        new_comment.ticket_id = ticket_id
+        new_comment.save()
+    return redirect('ticket-detail', ticket_id=ticket_id)
+
+
+class TagCreate(CreateView):
+    model = Tag
+    fields = '__all__'
+
+
+class TagList(ListView):
+    model = Tag
+
+
+class TagDetail(DetailView):
+    model = Tag
+
+
+class TagUpdate(UpdateView):
+    model = Tag
+    fields = ['name', 'color']
+
+
+class TagDelete(DeleteView):
+    model = Tag
+    success_url = '/tags/'
+
+
+def associate_tag(request, ticket_id, tag_id):
+    Ticket.objects.get(id=ticket_id).tags.add(tag_id)
+    return redirect('ticket-detail', ticket_id=ticket_id)
+
+
+def remove_tag(request, ticket_id, tag_id):
+    Ticket.objects.get(id=ticket_id).tags.remove(tag_id)
+    return redirect('ticket-detail', ticket_id=ticket_id)
